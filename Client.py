29ecc1ebd5f5db42eeba865a909b047b938a37ef
garjania <a.garjani@gmail.com>
2020-07-25 21:47:46 +0430

    Client Modified

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = 'localhost'
 PORT = 8080
@@ -10,15 +11,20 @@ class Client():
         self.sock = socket.socket()                     
         self.sock.connect(ADDR)
         self.id = None
+        self.chats = dict()
+        self.lock = threading.Lock()
 
     def send(self, message):
+        self.lock.acquire()
         data = str(message)
         self.sock.send(bytearray(data, 'utf-8'))
+        self.lock.release()
     
     def loggin(self, _id):
         self.id = _id
         message = {'type': 'LOGIN', 'id': self.id, 'reciever': '_Server'}
-        self.sock.send(message)
+        data = bytearray(str(message), 'utf-8')
+        self.sock.send(data)
         while True:
             data = self.sock.recv(BUFSIZE)
             if data: 
@@ -26,14 +32,43 @@ class Client():
         data = str(data, 'utf-8')
         data = eval(data)
         return data['body']
+    
+    def handle_client_info(self, data):
+        _id = data['id']
+        _num = data['MSSG_NUM']
+        self.lock.acquire()
+        message = self.chats[_id]['messages'][_num]
+        message['status'] = data['body']
+        self.lock.release()
+    
+    def send_info(self, _id, info):
+        message = {'id': _id}
 
-    def start(self):
+    def handle_message(self, data):
+        _id = data['id']
+        message = {'sender': _id, 'status': 'RECV', 'body': data['body']}
+        self.lock.acquire()
+        self.chats[_id]['messages'].append(message)
+        self.lock.release()
+
+    def handle_data(self, raw_data):
+        data = eval(raw_data)
+        if data['type'] == 'INFO':
+            if data['id'] != '_Server':
+                self.handle_client_info(data)
+        elif data['type'] == 'MSSG':
+            self.handle_message(data)
+
+    def recieve(self):
+        while True:
+            data = self.sock.recv(BUFSIZE)
+            if not data:
+                continue
+            data = str(data, 'utf-8')
+            self.handle_data(data)
+        self.sock.close()
         
-  
-# receive data from the server 
-print s.recv(1024) 
-# close the connection 
-s.close()
+    def start(self):
 
 if __name__=='__main__':
     
